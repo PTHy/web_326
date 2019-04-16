73203d2f193e73990eed932ef64c9cc1bb7a9635
Park Tae Hyeong <tae020117@gmail.com>
2019-04-16 09:47:20 +0900

    inital commit

--- src/main/java/kr/hs/dgsw/web_326/Domain/Comment.java
+++ src/main/java/kr/hs/dgsw/web_326/Domain/Comment.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class Comment {
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,5 @@
 let currentUser;
+let modifyContent;
 
 window.onload = () => {
     getComments();
@@ -33,61 +34,91 @@ function drawComments(comments) {
 }
 
 function drawComment(comment) {
+    const d = new Date(comment.created);
     console.log(comment);
     let buttonContent= '';
     if (comment.userId === currentUser) {
         buttonContent = `<div>
-                            '<button onclick=>삭제</button>
-'                            <button>수정</button>
+                            <button onclick="modifyComment(this, ${comment.id})">수정</button>
+                            <button onclick="deleteComment(this, ${comment.id})">삭제</button>
                         </div>`
     }
-    $('#comments').append(
+    $('#comments').prepend(
         `
-        <div>
-            <img class="comment-image" src="${(comment.storedPath === null) ? "/public/default.jpg" :  `/attachment/comment/${comment.id}`}" alt="사진">
+        <div id="comment${comment.id}">
+            <img class="comment-image" src="${(comment.storedPath === null) ? "/public/no-image.jpg" :  `/attachment/comment/${comment.id}`}" alt="사진">
             <div class="user-name">${comment.username}</div>
-            <div class="created">${comment.created}</div>
+            <div class="created">${d.getFullYear()}.${d.getMonth() + 1}.${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}</div>
             <div class="content">${comment.content}</div>
-            {}
+            ${buttonContent}
          </div>
        `
     )
 }
 
-async function modifyCommentSubmit(id) {
-    const modifyComment = {
-        id,
-        userId: 1,
-        content: $(`#comment${id}`).children('.modifyContent').children('.inputContent').val()
-    };
-    await $.ajax({
-        url: '/comment',
-        type: 'put',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(modifyComment),
-        success: (result) => {
-            if(result) {
-                /*
-                구현 필요
-                 */
+
+
+async function modifyComment(button, id) {
+    if (!chkLogin())
+        return;
+
+    const comment = $(`#comment${id}`);
+    if ($(button).text() === '수정') {
+        modifyContent = comment.children('.content').html();
+        console.log(`modify Content : ${modifyContent}`);
+        const input = `<input value="${modifyContent}">`
+        console.log(`input : ${input}`);
+        comment.children('.content').html(input);
+        // comment.find('input').focus();
+        $(button).text('확인');
+        $(button).next().text('취소');
+    } else if ($(button).text() === '확인') {
+        const modifyComment = {
+            id,
+            userId: currentUser,
+            content: comment.children('.content').find('input').val(),
+        };
+        await $.ajax({
+            url: '/comment',
+            type: 'put',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(modifyComment),
+            success: (result) => {
+                if(result) {
+                    alert('글이 수정되었습니다');
+                    comment.children('.content').html(result.content);
+                    $(button).text('수정');
+                    $(button).next().text('삭제');
+                }
             }
-        }
-    })
+        })
+    }
 }
 
-async function deleteComment(id) {
-    try {
-        await $.ajax({
-            url: `comment/${id}`,
-            type: 'delete',
-            success : (result) => {
-                if (result === true)
-                    $(`#comment${id}`).remove();
-            }
-        });
-    } catch (error) {
-        console.log(JSON.stringify(error));
+async function deleteComment(button, id) {
+    if (!chkLogin())
+        return;
+
+    if ($(button).text() === '삭제') {
+        try {
+            await $.ajax({
+                url: `comment/${id}`,
+                type: 'delete',
+                success : (result) => {
+                    if (result === true)
+                        $(`#comment${id}`).remove();
+                    alert('글이 삭제되었습니다');
+                }
+            });
+        } catch (error) {
+            console.log(JSON.stringify(error));
+        }
+    } else if ($(button).text() === '취소') {
+        const comment = $(`#comment${id}`);
+        comment.children('.content').html(modifyContent);
+        $(button).text('삭제');
+        $(button).prev().text('수정');
     }
 }
 
@@ -213,6 +244,9 @@ async function login() {
                     $('#user-name').text(result.username);
                     $('#user-email').text(result.email);
                     $('#user-joined').text(result.joined);
+
+                    $('#comments').children().remove();
+                    getComments();
                     changeMode();
                 } else {
                     alert("로그인 실패");
